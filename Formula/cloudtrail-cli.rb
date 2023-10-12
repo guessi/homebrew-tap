c4012fad9d0700ab0fc17fb3f52f5595c3cba6ea
guessi <guessi@gmail.com>
2023-10-12 17:59:20 +0800

    Update Formula/cloudtrail-cli

--- Formula/cloudtrail-cli.rb
+++ Formula/cloudtrail-cli.rb
@@ -1,29 +1,29 @@
 class CloudtrailCli < Formula
   desc "Blazing fast single purpose cli for CloudTrail log filtering"
   homepage "https://github.com/guessi/cloudtrail-cli"
-  version "1.0.5"
+  version "1.0.6"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.5/cloudtrail-cli-Darwin-arm64.tar.gz"
-      sha256 "abb31776061e1323b4e119445282badeae715cce542dc3b5f71e6205e94f3101"
+      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.6/cloudtrail-cli-Darwin-arm64.tar.gz"
+      sha256 "7b5d8add04187358fd77cba7b7a2865c37bceb7cd67fb89623933403af5e0f77"
     end
     on_intel do
-      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.5/cloudtrail-cli-Darwin-x86_64.tar.gz"
-      sha256 "7c023b97a7c9f3f31c843fb87fcc2e4d4dcf2fe90fa3155152b2b806fa8d147a"
+      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.6/cloudtrail-cli-Darwin-x86_64.tar.gz"
+      sha256 "07f63ca3d7bfe09dfce9f01b0a1fd7a0dd3b2f69a5e994c009038c2e69a89be0"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.5/cloudtrail-cli-Linux-arm64.tar.gz"
-      sha256 "11b1944d5f7686086d33abe14d5b06214920a9fff6edd6d3b2ab05e7bded6df7"
+      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.6/cloudtrail-cli-Linux-arm64.tar.gz"
+      sha256 "0c346440bc2d250a3033978f4c7aa475255f15a14f914933a9ade3d9b27903ef"
     end
 
     on_intel do
-      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.5/cloudtrail-cli-Linux-x86_64.tar.gz"
-      sha256 "b2d6ce25f70733f42a39f6ba07224ccb5ef8a16282346474ea1e483f4edfbae0"
+      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.6/cloudtrail-cli-Linux-x86_64.tar.gz"
+      sha256 "b6c73d0949514ec75e8a767762b62943bff27771bcdc3c434a8ed044f7f7939c"
     end
   end
 
