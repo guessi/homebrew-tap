36fc16b1181c1954ca4b4f77bbcaa79e69cfea75
guessi <guessi@gmail.com>
2023-10-12 18:21:02 +0800

    Update Formula/eks-ami-finder

--- Formula/eks-ami-finder.rb
+++ Formula/eks-ami-finder.rb
@@ -1,29 +1,29 @@
 class EksAmiFinder < Formula
   desc "Retrieve Amazon EKS AMI with filters"
   homepage "https://github.com/guessi/eks-ami-finder"
-  version "1.0.3"
+  version "1.0.4"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/guessi/eks-ami-finder/releases/download/v1.0.3/eks-ami-finder-Darwin-arm64.tar.gz"
-      sha256 "04051917241c7d5f1ed5391013d630e0d23f6e5431506f30d8d501bd7d2a60e6"
+      url "https://github.com/guessi/eks-ami-finder/releases/download/v1.0.4/eks-ami-finder-Darwin-arm64.tar.gz"
+      sha256 "1edd5fe3bddd4cc35308ea12c538ae810146c1f5dcf8299f1d6ab18ea26f2a62"
     end
     on_intel do
-      url "https://github.com/guessi/eks-ami-finder/releases/download/v1.0.3/eks-ami-finder-Darwin-x86_64.tar.gz"
-      sha256 "2864b86818d9416fc7f0ee19aa1a6cafc31c5b798889562d52aa9fb69c1b16a5"
+      url "https://github.com/guessi/eks-ami-finder/releases/download/v1.0.4/eks-ami-finder-Darwin-x86_64.tar.gz"
+      sha256 "8e096c3f438c563762161feae0be73153147236c8a1b4492265b3d254b9631b3"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/guessi/eks-ami-finder/releases/download/v1.0.3/eks-ami-finder-Linux-arm64.tar.gz"
-      sha256 "8a9f6ce5f3b2d5e3fd55e183a58bbea52b957c6908d47c88870160659604f24d"
+      url "https://github.com/guessi/eks-ami-finder/releases/download/v1.0.4/eks-ami-finder-Linux-arm64.tar.gz"
+      sha256 "86e25989962e4d0082933aeea509fc4ebac42204d91aa44c249cdfe07efa5d18"
     end
 
     on_intel do
-      url "https://github.com/guessi/eks-ami-finder/releases/download/v1.0.3/eks-ami-finder-Linux-x86_64.tar.gz"
-      sha256 "c510c00e23b4f3872b3a084df3bd34726b26dff22b68bdbad6f179556db5c7fe"
+      url "https://github.com/guessi/eks-ami-finder/releases/download/v1.0.4/eks-ami-finder-Linux-x86_64.tar.gz"
+      sha256 "5d7aa7fad66e0b9ca12af4c3b6abf6b0c2791c08f9809dc60e9d9d42ca7d00c6"
     end
   end
 
