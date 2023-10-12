0784ebea57cbcc0f922c66051174d41a9e754e5b
guessi <guessi@gmail.com>
2023-10-12 18:21:53 +0800

    Update Formula/cloudtrail-cli

--- Formula/cloudtrail-cli.rb
+++ Formula/cloudtrail-cli.rb
@@ -1,29 +1,29 @@
 class CloudtrailCli < Formula
   desc "Blazing fast single purpose cli for CloudTrail log filtering"
   homepage "https://github.com/guessi/cloudtrail-cli"
-  version "1.0.6"
+  version "1.0.7"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.6/cloudtrail-cli-Darwin-arm64.tar.gz"
-      sha256 "7b5d8add04187358fd77cba7b7a2865c37bceb7cd67fb89623933403af5e0f77"
+      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.7/cloudtrail-cli-Darwin-arm64.tar.gz"
+      sha256 "6ed490c30e362122bdc1c53b5c75bf1342e112af790b3527e6d153587f37bf77"
     end
     on_intel do
-      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.6/cloudtrail-cli-Darwin-x86_64.tar.gz"
-      sha256 "07f63ca3d7bfe09dfce9f01b0a1fd7a0dd3b2f69a5e994c009038c2e69a89be0"
+      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.7/cloudtrail-cli-Darwin-x86_64.tar.gz"
+      sha256 "f828dec1202dc353d8e97e65b4d921961c8788f7c763d4d505f2a62b2c88a005"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.6/cloudtrail-cli-Linux-arm64.tar.gz"
+      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.7/cloudtrail-cli-Linux-arm64.tar.gz"
       sha256 "0c346440bc2d250a3033978f4c7aa475255f15a14f914933a9ade3d9b27903ef"
     end
 
     on_intel do
-      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.6/cloudtrail-cli-Linux-x86_64.tar.gz"
-      sha256 "b6c73d0949514ec75e8a767762b62943bff27771bcdc3c434a8ed044f7f7939c"
+      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.7/cloudtrail-cli-Linux-x86_64.tar.gz"
+      sha256 "2009200a2e3ca97012bd8ff2db8f6c98e488181f3f6fdeeb382d70e9f22a2da3"
     end
   end
 
