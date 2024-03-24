4a9efd8d7f5ae7ee42242941fa15bac021644c18
guessi <guessi@gmail.com>
2024-03-24 13:34:49 +0800

    Update Formula/cloudtrail-cli

--- Formula/cloudtrail-cli.rb
+++ Formula/cloudtrail-cli.rb
@@ -1,29 +1,29 @@
 class CloudtrailCli < Formula
   desc "Blazing fast single purpose cli for CloudTrail log filtering"
   homepage "https://github.com/guessi/cloudtrail-cli"
-  version "1.0.10"
+  version "1.0.11"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.10/cloudtrail-cli-Darwin-arm64.tar.gz"
-      sha256 "f785e8c3863afa7e7a04c1b228e70a63c7a4505c4e78fa589439199481c1cfa1"
+      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.11/cloudtrail-cli-Darwin-arm64.tar.gz"
+      sha256 "b9841f7e3249cd50975b6fd7f18ce6cb304eb60f345f7ba5404a5d5104a4ed65"
     end
     on_intel do
-      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.10/cloudtrail-cli-Darwin-x86_64.tar.gz"
-      sha256 "c4017d5a9bfbb4abf3a7bcab0b669a3e2fa78f7eb697972c7fd4202e37bed0c4"
+      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.11/cloudtrail-cli-Darwin-x86_64.tar.gz"
+      sha256 "61c4062e8c42d09de7fc5b8fec8d18550f634aaa92f06ed24baec2000fcd376d"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.10/cloudtrail-cli-Linux-arm64.tar.gz"
-      sha256 "9c3768200ec72617121750c32c62d4a07ddf2890c476682075c80cc85f78fc63"
+      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.11/cloudtrail-cli-Linux-arm64.tar.gz"
+      sha256 "be709cec9e6533fb39125e330b2abdb0ae9f75264b555d1a4e91ec711dc25ca0"
     end
 
     on_intel do
-      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.10/cloudtrail-cli-Linux-x86_64.tar.gz"
-      sha256 "fc1914d635e1a667df5c5ee658c51ccca33930fd5dd3f04316ebd4fe66d3faf3"
+      url "https://github.com/guessi/cloudtrail-cli/releases/download/v1.0.11/cloudtrail-cli-Linux-x86_64.tar.gz"
+      sha256 "9af8f7c039ca829a0071ceead1232830b7e5b781ae58f5be98a04f4d8e31cf1f"
     end
   end
 
