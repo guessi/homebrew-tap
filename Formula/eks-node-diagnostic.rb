67f5f48d8ffbca04bb7dd54e9db7694ae66bbe30
guessi <guessi@gmail.com>
2025-07-03 08:34:09 +0800

    Update Formula/eks-node-diagnostic

--- Formula/eks-node-diagnostic.rb
+++ Formula/eks-node-diagnostic.rb
@@ -1,29 +1,29 @@
 class EksNodeDiagnostic < Formula
   desc "Friendly NodeDiagnostic generator with no Python/SDK required"
   homepage "https://github.com/guessi/eks-node-diagnostic"
-  version "1.4.1"
+  version "1.4.2"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/guessi/eks-node-diagnostic/releases/download/v1.4.1/eks-node-diagnostic-Darwin-arm64.tar.gz"
-      sha256 "099662195bbf0afd34bf599955ed0a4cf5302c1668029cedee4bab339588fe0d"
+      url "https://github.com/guessi/eks-node-diagnostic/releases/download/v1.4.2/eks-node-diagnostic-Darwin-arm64.tar.gz"
+      sha256 "4b917147469bb8dec543c27bb53be0da4f28250a1c414e62654e81679102003e"
     end
     on_intel do
-      url "https://github.com/guessi/eks-node-diagnostic/releases/download/v1.4.1/eks-node-diagnostic-Darwin-x86_64.tar.gz"
-      sha256 "7771c5f6e6a4007dde5d303554bd2ddd3625b9935ae04a734b8cdcda7bbf0618"
+      url "https://github.com/guessi/eks-node-diagnostic/releases/download/v1.4.2/eks-node-diagnostic-Darwin-x86_64.tar.gz"
+      sha256 "43f6694d9afb69ff3dfe99f7c946c0201f97c38387e5a862612bb9ac7f7f0d1d"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/guessi/eks-node-diagnostic/releases/download/v1.4.1/eks-node-diagnostic-Linux-arm64.tar.gz"
-      sha256 "73e99883a4806bbd25886b7e15c4893e843ea2040b8c701a636fdfdef8388ccf"
+      url "https://github.com/guessi/eks-node-diagnostic/releases/download/v1.4.2/eks-node-diagnostic-Linux-arm64.tar.gz"
+      sha256 "b4ef23478e1f331ecc26a9235e05617ceb48e0169ad3ca3ec0e8b52f4e71ea91"
     end
 
     on_intel do
-      url "https://github.com/guessi/eks-node-diagnostic/releases/download/v1.4.1/eks-node-diagnostic-Linux-x86_64.tar.gz"
-      sha256 "dc411e30311e47668f1dbc8b96054cc45df9223d2df7e9b61db5d17b53274ac7"
+      url "https://github.com/guessi/eks-node-diagnostic/releases/download/v1.4.2/eks-node-diagnostic-Linux-x86_64.tar.gz"
+      sha256 "3c6d88db9dce33392a7ea0f254795b9d6d2ee54217315843924ba5ef643185af"
     end
   end
 
